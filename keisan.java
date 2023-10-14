93cb75edc58def815197b3b52fb3e8a65e433bc3
uetaka0805 <147686214+uetaka0805@users.noreply.github.com>
2023-10-14 10:36:55 +0900

    Add files via upload

---
+++ keisan.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		int a;
+		int b;
+		a = 20;
+		b = a + 5;
+		a = 0x11;
+		b = 0b0011;
+
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
